e1d615d1e8890ccff67c6d1752fbdb4ef0bf824b
KoolieAid <erkgo.eg@gmail.com>
2022-02-27 01:53:54 +0800

    AutoComplete Play
    JDA updated to alpha 9
    New AutoSlashCommandInterface
    Play now can autoplete query
    Context is accepts IReplyCallback instead of ContextInteraction
    Command scanning now frees the result for memory management

--- src/main/java/com/koolie/bot/richter/commands/music/Play.java
+++ src/main/java/com/koolie/bot/richter/commands/music/Play.java
@@ -1,19 +1,22 @@
 package com.koolie.bot.richter.commands.music;
 
 import com.koolie.bot.richter.MusicUtil.MusicManager;
+import com.koolie.bot.richter.commands.AutoSlashCommand;
 import com.koolie.bot.richter.commands.ContextCommand;
 import com.koolie.bot.richter.commands.TextCommand;
 import com.koolie.bot.richter.objects.Context;
 import net.dv8tion.jda.api.entities.AudioChannel;
 import net.dv8tion.jda.api.entities.Message;
 import net.dv8tion.jda.api.events.interaction.command.MessageContextInteractionEvent;
+import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
 import net.dv8tion.jda.api.exceptions.InsufficientPermissionException;
+import net.dv8tion.jda.api.interactions.commands.CommandAutoCompleteInteraction;
 import org.jetbrains.annotations.NotNull;
 
 import java.net.MalformedURLException;
 import java.net.URL;
 
-public class Play implements TextCommand, ContextCommand {
+public class Play implements TextCommand, ContextCommand, AutoSlashCommand {
     public Play() {
     }
 
@@ -45,6 +48,12 @@ public class Play implements TextCommand, ContextCommand {
     @Override
     public String[] getAliases() {
         return new String[]{"p"};
+    }
+
+    @NotNull
+    @Override
+    public String getEffectiveCommand() {
+        return "play";
     }
 
     @Override
@@ -138,5 +147,52 @@ public class Play implements TextCommand, ContextCommand {
             musicManager.eventListener.nextTrack();
         }
         event.getGuild().getAudioManager().setSelfDeafened(true);
+    }
+
+    @Override
+    public void onSlash(SlashCommandInteractionEvent event) {
+        AudioChannel vChannel = event.getMember().getVoiceState().getChannel();
+        if (vChannel == null) {
+            event.reply("Bro. You are not in a voice channel").setEphemeral(true).queue();
+            return;
+        }
+
+        event.getChannel().sendTyping().queue();
+        String query = event.getInteraction().getOption("query").getAsString();
+
+        try {
+            URL url = new URL(query);
+            if (!url.getHost().equalsIgnoreCase("open.spotify.com")) throw new MalformedURLException();
+
+        } catch (MalformedURLException e) {
+            if (!query.matches("^((?:https?:)?\\/\\/)?((?:www|m)\\.)?((?:youtube\\.com|youtu.be))(\\/(?:[\\w\\-]+\\?v=|embed\\/|v\\/)?)([\\w\\-]+)(\\S+)?$")
+                    && !query.startsWith("direct:")) {
+                query = "ytsearch:" + query;
+            }
+        }
+
+        if (query.startsWith("direct:")) {
+            query = query.substring("direct:".length());
+        }
+
+        MusicManager.loadToGuild(new Context(event), query);
+
+        MusicManager musicManager = MusicManager.of(event.getGuild());
+        if (musicManager.audioPlayer.isPaused()) {
+            musicManager.audioPlayer.setPaused(false);
+        }
+        try {
+            event.getGuild().getAudioManager().openAudioConnection(vChannel);
+        } catch (InsufficientPermissionException e) {
+            event.reply("I can't seem to connect to that channel").setEphemeral(true).queue();
+            musicManager.eventListener.queue.clear();
+            musicManager.eventListener.nextTrack();
+        }
+        event.getGuild().getAudioManager().setSelfDeafened(true);
+    }
+
+    @Override
+    public void completeOption(CommandAutoCompleteInteraction interaction) {
+        MusicManager.autoComplete(interaction.getFocusedOption().getValue(), interaction);
     }
 }
--- src/main/java/com/koolie/bot/richter/objects/Context.java
+++ src/main/java/com/koolie/bot/richter/objects/Context.java
@@ -1,55 +1,57 @@
 package com.koolie.bot.richter.objects;
 
 import net.dv8tion.jda.api.entities.*;
-import net.dv8tion.jda.api.interactions.commands.context.MessageContextInteraction;
+import net.dv8tion.jda.api.interactions.callbacks.IReplyCallback;
 import net.dv8tion.jda.api.requests.RestAction;
 
 public class Context {
     Message message;
-    MessageContextInteraction contextMessage;
+    IReplyCallback interaction;
 
     public Context(Message message) {
         this.message = message;
-        contextMessage = null;
     }
 
-    public Context(MessageContextInteraction contextMessage) {
-        this.contextMessage = contextMessage;
-        message = null;
+    public Context(IReplyCallback interaction) {
+        this.interaction = interaction;
     }
 
     public RestAction reply(String message) {
         if (this.message != null) {
             return this.message.reply(message);
         }
-        return this.contextMessage.reply(message).setEphemeral(true);
+        return this.interaction.reply(message).setEphemeral(true);
     }
 
     public RestAction replyEmbeds(MessageEmbed embed) {
         if (this.message != null) {
             return this.message.replyEmbeds(embed);
         }
-        return this.contextMessage.replyEmbeds(embed).setEphemeral(true);
+        return this.interaction.replyEmbeds(embed).setEphemeral(true);
     }
 
     public MessageChannel getChannel() {
         if (this.message != null) {
             return this.message.getChannel();
         }
-        return this.contextMessage.getChannel();
+        return this.interaction.getMessageChannel();
     }
 
     public Guild getGuild() {
         if (this.message != null) {
             return this.message.getGuild();
         }
-        return this.contextMessage.getGuild();
+        return this.interaction.getGuild();
     }
 
     public User getAuthor() {
         if (this.message != null) {
             return this.message.getAuthor();
         }
-        return this.contextMessage.getUser();
+        return this.interaction.getUser();
+    }
+
+    public User getUser() {
+        return getAuthor();
     }
 }
